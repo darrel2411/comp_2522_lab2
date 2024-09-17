d4f9077d596744382e4fdbd594029d3c36996d6a
Mana1422 <manasesvillalobos80@gmail.com>
2024-09-16 17:21:59 -0700

    Started creating the Healer Creature for the bonus question.

---
+++ src/code/ca/bcit/comp2522/fantasy/CreatureHealer.java
@@ -0,0 +1,17 @@
+package ca.bcit.comp2522.fantasy;
+
+public class CreatureHealer  extends Creature {
+
+    /**
+     * Constructor for the object
+     * @param name represents the name of the creature
+     * @param dateOfBirth represents the birthday of the
+     */
+    public CreatureHealer(final String name,
+                          final Date dateOfBirth)
+    {
+        super(name, dateOfBirth);
+    }
+
+
+}
